dbd9c9a93627d787b0f6d7bf0735d66afa525871
LilShah <34099012+LilShah@users.noreply.github.com>
2023-05-26 01:18:28 +0500

    add Dummy api's spec

--- api/v1alpha1/dummy_types.go
+++ api/v1alpha1/dummy_types.go
@@ -20,22 +20,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // DummySpec defines the desired state of Dummy
 type DummySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Dummy. Edit dummy_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Message string `json:"message"`
 }
 
 // DummyStatus defines the observed state of Dummy
 type DummyStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 //+kubebuilder:object:root=true
